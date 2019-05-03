7f6f86978752272f66839750d06994aa97ed2ff7
Diletta Lanini <lanini.1456343@studenti.uniroma1.it>
2019-05-03 12:44:41 +0200

    PrimomoLanini

--- OnLed_1/switch_dimmer.c
+++ OnLed_1/switch_dimmer.c
@@ -5,7 +5,7 @@
 #include <avr/io.h>
 #include "switch_dimmer.h"
 
-#define TCCRA_MASK (1<<WGM30)|(1<<COM3C0)|(1<<COM3C1)
+#define TCCRA_MASK (1<<WGM30)|(1<<COM3B0)|(1<<COM3B1)
 #define TCCRB_MASK ((1<<WGM32)|(1<<CS30))   
 
 //Pin fisici che supportano pwm 26-25-24-23-18-17-16-15-5-1-7-6
@@ -20,19 +20,19 @@ void Led_On(void)
 
 void Led_Dimmer(void)
 {
-	//Set TCCR3
+	//Set TCCR3 and channel B
  	TCCR3A=TCCRA_MASK;
 	TCCR3B=TCCRB_MASK;
 	//Set Compare Register
-	OCR3CH=0;
-	OCR3CL=1;
+	OCR3BH=0;
+	OCR3BL=1;
 	//PIN 6 porta E, collegato all'OSC3
 	const uint8_t mask=(1<<4);
 	DDRE |= mask;
 	uint8_t intensity=0;
 	while(1)
 	{
-		OCR3CL=intensity; 
+		OCR3BL=intensity; 
 		_delay_ms(100);
 		intensity+=8;
 	}
