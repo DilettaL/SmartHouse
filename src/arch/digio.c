9cd787c4e363ea0cfa6fad096783cc226c05b614
Diletta Lanini <lanini.1456343@studenti.uniroma1.it>
2019-07-05 11:26:39 +0200

    aggiornamento digio (digitalinput)

--- src/arch/digio.c
+++ src/arch/digio.c
@@ -23,25 +23,26 @@ void ledDimmer(uint8_t pin, uint8_t intensity)
 		_delay_ms(100);
 		intensity+=8;
 	}
-/*
-#define TCCRA_MASK (1<<WGM30)|(1<<COM3B0)|(1<<COM3B1)
-#define TCCRB_MASK ((1<<WGM32)|(1<<CS30))   
-//Set TCCR3 and channel B (pag 82)
- 	TCCR3A=TCCRA_MASK;
-	TCCR3B=TCCRB_MASK;
-	//Set Compare Register
-	OCR3BH=0;
-	OCR3BL=1;
-	//PIN 6 porta E, collegato all'OSC3
-	const uint8_t mask=(1<<4);
-	DDRE |= mask;
-	uint8_t intensity=0;
+}
+
+void digitalInput(void)
+{
+
+//UTILIZZARE SOLO PIN CHE UNANO PIN CHANGE!!
+
+  // this initializes the printf/uart thingies
+//  printf_init(); 
+
+	const Pin* mapping=pins+pin;
+	uint8_t mask=1<<mapping->bit;
+	*(mapping->dir_register)&=~ mask;
+	*(mapping->out_register)|= mask;
 	while(1)
 	{
-		OCR3BL=intensity; 
-		_delay_ms(100);
-		intensity+=8;
+		//uint8_t result= *(mapping->in_register);		
+		int key=(PINB&mask)==0; // we extract the bit value of the 6th bit
+		//printf("switch %02x, %d\n", (int) PORTB, key);
+		//_delay_ms(500); // from delay.h, wait 1 sec
 	}
-*/
 }
 
