3784b5da21f6dc9ff7e8fd5ce41e71f1586b3018
Diletta Lanini <lanini.1456343@studenti.uniroma1.it>
2019-07-17 16:16:39 +0200

    LaniniPrimomo-Led/Dimmer

--- src/arch/digio.c
+++ src/arch/digio.c
@@ -26,7 +26,7 @@ void ledDimmer(uint8_t pin, uint8_t intensity)
 	*(mapping->oc_register_high)=0;
 	uint8_t mask=1<<mapping->bit;
 	*(mapping->dir_register) |= mask;
-	while(light<intensity)
+	while(1)
 	{
 		*(mapping->oc_register_low)=light;
 		_delay_ms(100);
