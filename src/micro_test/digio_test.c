c8a7b9ad421ab87b573f55110f68a3b54b9ec4f7
Luca <primomo.luca@gmail.com>
2019-07-18 22:05:30 +0200

    allineamento

--- src/micro_test/digio_test.c
+++ src/micro_test/digio_test.c
@@ -1,9 +1,14 @@
 #include <avr/io.h>
 #include "digio.h"
 #include <util/delay.h>
-
+/*Per correttezza questi test andrebbero riscritti facendo in modo di poter leggere su UART i valori attesi senza necessità di dover collegare led ?? */
 int main(int argc, char *argv[])
 {
+//Test ledDimmer
+
+
+
+/*
 //Test ledOn e led Off su pin da 2 a 13:
 	while(1)
 	{
@@ -15,5 +20,6 @@ int main(int argc, char *argv[])
 			_delay_ms(500);
 		}
 	}
+*/
 	return 0;
 }
