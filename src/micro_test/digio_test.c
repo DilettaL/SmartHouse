0460e6984d711b179c1eccbecd2c813f40916b45
Luca <primomo.luca@gmail.com>
2019-07-18 17:30:50 +0200

    microtest digio_test.c - funzioni ledOn ledOff ok

--- src/micro_test/digio_test.c
+++ src/micro_test/digio_test.c
@@ -7,15 +7,12 @@ int main(int argc, char *argv[])
 //Test ledOn e led Off su pin da 2 a 13:
 	while(1)
 	{
-	int i;
-		for(i=2; i<13; i++)
+		for(int i=2; i<14; i++)
 		{
-			_delay_ms(500);
 			ledOn(i);
 			_delay_ms(500);
 			ledOff(i);
-			if(i==12)
-			{i=2;}
+			_delay_ms(500);
 		}
 	}
 	return 0;
