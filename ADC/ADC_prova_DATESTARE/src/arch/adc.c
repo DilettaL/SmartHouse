2e2adfb8bafbaf1ff6b980d40db8549d07a08747
Diletta Lanini <lanini.1456343@studenti.uniroma1.it>
2019-09-24 12:34:00 +0200

    PrimomoLanini-ADC test

--- ADC/ADC_prova_DATESTARE/src/arch/adc.c
+++ ADC/ADC_prova_DATESTARE/src/arch/adc.c
@@ -13,17 +13,22 @@
 
 void adc_init()
 {
-	SREG |= (1<<7); 
+//SREG |= (1<<7); 
 	//Defalut right adjust mode
 /*PROVVISORIO*/	//Reference selection (supply) =1.1V internal e capacità esterna su AREF
 	ADMUX= (1<<REFS1)|(0<<REFS0);
+	//ADC Enable
+	ADCSRA=(1<<ADEN);
+
+//Si configura il prescaler (per ora non si definisce un valore)
+/*PROVVISORIO*///ADCSRA=(<<ADPS2)|(<<ADPS1)|(<<ADPS0);
+
+
 	//If we use Single Mode Conversion we must delete next lines
 	//Set autotrigger mode (free running, ecc..), but previously we able the ADTS bit selection with ADCSRA register
 /*PROVVISORIO*///ADCSRA=(1<<ADATE);	
 	//da controllare se giusto inserirlo qui
 /*PROVVISORIO*///ADCSRB=(<<ADTS2)|(<<ADTS1)|(<<ADTS0);
-//Si configura il prescaler (per ora non si definisce un valore)
-/*PROVVISORIO*///ADCSRA=(<<ADPS2)|(<<ADPS1)|(<<ADPS0);
 }
 
 float adc_run(uint8_t pin, uint8_t numb_samples)
@@ -42,26 +47,32 @@ float adc_run(uint8_t pin, uint8_t numb_samples)
 	//Configurare eventuali trigger
 //Se si è nella modalità autotrigger bisogna abilitare questo bit a 1
 /*PROVVISORIO*///ADCSRA=(1<<ADATE);
-	//ADC Enable
-	ADCSRA=(1<<ADEN);
 //Non so se va applicato qui, si abilita l'interrupt che definisce il termine della conversione
-/*PROVVISORIO*/ADCSRA=(1<<ADIE);
+/*PROVVISORIO*/
 	//start conversion
 //se si è in free running mode (lo si abilita una volta e poi va in automatico)
 /*PROVVISORIO*///ADCSRA=(1<<ADSC);
-	//Definire quanti campioni per misura si vogliono acquisire
+
+/*Test: tolto per vedere se è automatico*/
+//ADCSRA=(1<<ADIE);
 	for(count=0; count<numb_samples; count++)
 	{
+		while( ADCSRA & (1<<ADSC) );
+		result[count]=ADC;
+		printf("result[%d]= %f\n", count, result[count]);
+
+
+
 		//start conversion
 //si posiziona qui se conversione in single mode ogni volta deve essere riabilitato
-/*PROVVISORIO*/	ADCSRA=(1<<ADSC);
+/*PROVVISORIO*/	//ADCSRA=(1<<ADSC);
 		//Check conversion end
-		while(ADIF!=1);
+		//while(ADIF!=1);
 		//{
-			result[count]=0;
+		/*	result[count]=0;
 			result[count]+=ADCL;
 			result[count]+=ADCH;
-			//Conversion is complete, now we can read and save the data
+		*/	//Conversion is complete, now we can read and save the data
 			//ADCL register
 			
 				//Operations for single mode conversion and right adjust configuration
@@ -71,10 +82,9 @@ float adc_run(uint8_t pin, uint8_t numb_samples)
 				//Operations for single mode conversion and right adjust configuration
 //Per differential mode conversion o left adjust mode ----> operazioni diverse nel caso (pag 280)
 /*DA CONTROLLARE ADCL[COUNT]POTREBBE NON ESSERE GIUSTO*///
-		}
-	printf("result[%d]= %f\n", count, result[count]);
+		//}
 	//Pulizia eventuali registri
-//	}
+	}
 	return result[numb_samples];	
 }
 
