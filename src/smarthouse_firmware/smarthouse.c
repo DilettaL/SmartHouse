1be42b7868cc859a7371e46798f25663fb022181
Ubuntu 64-bit <diletta.lanini@gmail.com>
2019-10-10 03:29:51 -0700

    thread introduction

--- src/smarthouse_firmware/smarthouse.c
+++ src/smarthouse_firmware/smarthouse.c
@@ -1,5 +1,6 @@
 //Standard Libraries
 #include <stdio.h>
+#include <pthread.h>
 //Low Level Libraries
 #include "eeprom.h"
 #include "adc.h"
@@ -17,10 +18,24 @@ int main (int argc, char argv[])
 	digio_init();
 	//Inizializzazione funzioni alto livello //uart_init la inizializzo in Smarthouse_comm_init()
 	Smarthouse_comm_init();
+	//Threads creations
+	pthread_t keyboard, serial;
+	pthread_attr_t attr_keyboard, attr_serial;
+	pthread_attr_init(&attr_keyboard);
+	pthread_attr_init(&attr_serial);
+	phtread_create(&keyboard, &attr_keyboard, listen_keyboard, NULL); 
+	serial_resultphtread_create(&serial, &attr_serial, listen_serial, NULL); 
+	void *keyboard_result, *serial_result;
+
 	while(exit)
 	{	
-		//Orazio Shell	
-		Smarthouse_shellStart();
+		while( (pthread_join(keyboard, &keyboard_result)!=0) || (pthread_join(serial, &serial_result)!=0) )
+		{
+			//Orazio Shell	
+//			Smarthouse_shellStart()
+		}
 	}
+	pthread_attr_destroy(&attr_keyboard);
+  	pthread_attr_destroy(&attr_serial);
 	return 0;
 }
