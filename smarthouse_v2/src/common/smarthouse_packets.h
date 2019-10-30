9541296aa3f72436ad0c7da415b8dc283aa0a537
Ubuntu 64-bit <diletta.lanini@gmail.com>
2019-10-30 08:54:02 -0700

    test_v2

--- smarthouse_v2/src/common/smarthouse_packets.h
+++ smarthouse_v2/src/common/smarthouse_packets.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define DIGITAL_MAX 8
+#define ANALOG_MAX 8
 
 typedef enum
 {
@@ -13,9 +14,16 @@ typedef enum
 typedef struct
 {
 	DigitalType type;
-	uint8_t led[DIGITAL_MAX];
+	uint8_t ledOn[DIGITAL_MAX];
+	uint8_t ledOff[DIGITAL_MAX];
 	uint8_t dimmer[DIGITAL_MAX];
 	uint8_t input[DIGITAL_MAX];
 	uint8_t intensity;
 //	uint8_t result[DIGITAL_MAX];
 } DigitalConfig;
+
+typedef struct
+{
+	uint8_t Adc[ANALOG_MAX];
+	uint8_t samples;	
+} AnalogConfig;
--- smarthouse_v2/src/smarthouse_firmware/smarthouse.c
+++ smarthouse_v2/src/smarthouse_firmware/smarthouse.c
@@ -4,6 +4,18 @@
 
 int main(int argc, char **argv)
 {
+	DigIO_init();
+	Adc_init();
+	PWM_init();
+/*	AnalogConfig test;
+	for(int i=0; i<ANALOG_MAX; i++)
+	{
+		if(i=10) {test->Adc[i]=1;	}
+		else{test->Adc[i]=0;}
+	}	
+	test->samples=100;
+	Analog_init(test);
+*/
 	DigitalConfig test;
 	test->type= DigitalLed;
 	for(int i=0; i<DIGITAL_MAX; i++)
@@ -21,49 +33,80 @@ PacketStatus Digital_init(DigitalConfig digital)
 	switch(digital->type)
 	{
 		case DigitalLed:
-			Led(digital->*led);
+			Led(digital->*ledOn);
 		case DigitalDimmer:
 			Dimmer(digital->*dimmer);
 		case DigitalInput:
-			InputDigital(digital->*input);
+			InputDigital(digital->*input, digital->intensity);
+		case DigitalOff:
+			LedOff(digital->*ledOff);
 		default:
 			return -1;
 	}
 	return Success;
 }
 
-void Led(uint8_t *pin)
+void LedOn(uint8_t *pin)
 {
-	DigIO_init();
 	for(int i=0; i<DIGITAL_MAX; i++)
 	{
-		DigIO_setDirection(i, Output);
-		DigIO_setValue(i, led[i]);
+		if(pin[i]==1)
+		{
+			DigIO_setDirection(i, Output);
+			DigIO_setValue(i, 1);
+		}
 	}
 }
 
-void Dimmer(uint8_t *pin)
+void LedOff(uint8_t *pin)
+{
+	for(int i=0; i<DIGITAL_MAX; i++)
+	{
+		if(pin[i]==1)
+		{
+			DigIO_setDirection(i, Output);
+			DigIO_setValue(i, 0);
+		}
+	}
+}
+
+void Dimmer(uint8_t *pin, uint8_t intensity)
 {	
-	PWM_init();
-	PWM_enable(pin, 1);
-	PWM_setDutyCycle(pin, 10);
-	delayMs(100);
+	for(int i=0; i<DIGITAL_MAX; i++)
+	{
+		if(pin[i]==1)
+		{
+			PWM_enable(i, 1);
+			PWM_setDutyCycle(i, intensity);
+			delayMs(100);
+		}
+	}
 }
 
-void InputDigital(uint8_t pin)
+void InputDigital(uint8_t *pin)
 {
-	DigIO_init();
-	DigIO_setDirection(pin, Input);
-	DigIO_setValue(pin, 1);
-	uint8_t key=DigIO_getValue(pin);
-	printf("%d", key);	
+	for(int i=0; i<DIGITAL_MAX; i++)
+	{
+		if(pin[i]==1)
+		{
+			DigIO_setDirection(i, Input);
+			DigIO_setValue(i, 1);
+			uint8_t key=DigIO_getValue(i);
+			printf("%d", key);
+		}	
+	}
 }
 
-PacketStatus Analog_init(void)
+PacketStatus Analog_init(AnalogConfig analog)
 {
-	Adc_init();
-	SetAdc(4);
-	RunAdc(10);
+	for(int i=0; i<ANALOG_MAX; i++)
+	{
+		if(analog->Adc[i]==1)
+		{	
+			SetAdc(i);
+		}
+		RunAdc(analog->samples);
+	}
 	return Success;
 }
 */
