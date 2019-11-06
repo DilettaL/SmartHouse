3600cf87bdd0649a224f2b294f80b3fa7ec14540
Luca Primomo <primomo.luca@gmail.com>
2019-11-06 18:34:31 +0100

    sistemazione common-mancano pacchetti definitivi

--- smarthouse_v2/src/common/packet_header.h
+++ smarthouse_v2/src/common/packet_header.h
@@ -5,9 +5,8 @@ typedef uint8_t PacketType;
 typedef uint8_t PacketSize;
 typedef uint16_t PacketSeq;
 #define PACKET_SIZE_ANY 0xFF	//MI PERMETTE DI EFFETTUARE CONTROLLI
-
 #define PACKET_TYPE_MAX 5        //PERCHE' NE HO PER ORA 3 this is the maximum number of different packet types
-//#define PACKET_SIZE_MAX 254       // maximum length of a packet, including the header and        
+#define PACKET_SIZE_MAX 254       // maximum length of a packet, including the header and        
 
 #pragma pack(push, 1)
 typedef struct
--- smarthouse_v2/src/common/packet_operations.h
+++ smarthouse_v2/src/common/packet_operations.h
@@ -29,12 +29,20 @@ typedef enum
 	SyncChecksum=7
 } PacketStatus;
 
+struct PacketOperations;
+struct PacketHandler;
+
+typedef PacketStatus  (*PacketFn)(PacketHeader*, void* args);
+typedef PacketHeader*  (*PacketGenFn)(PacketType type,
+                                      PacketSize size,
+                                      void* args);
+
 typedef struct PacketOperations 
 {
 	PacketType type;
 	PacketSize size; // expected size of the payload, -1 if error;
 	PacketGenFn initialize_buffer_fn;
-//  void* initialize_buffer_args;
-//  PacketFn on_receive_fn;
-//  void* on_receive_args;
+	void* initialize_buffer_args;
+	PacketFn on_receive_fn;
+	void* on_receive_args;
 } PacketOperations;
