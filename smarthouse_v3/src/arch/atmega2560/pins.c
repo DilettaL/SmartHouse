c89495a13107d1c24eff2a133202c024bc3737b3
Luca Primomo <primomo.luca@gmail.com>
2019-11-12 20:06:57 +0100

    nel crare v3 abbiamo preso direttamente preso la cartella arch di Grisetti ma noi abbiamo aggiunto delle cose. Corretti errori. ARCH funziona!

--- smarthouse_v3/src/arch/atmega2560/pins.c
+++ smarthouse_v3/src/arch/atmega2560/pins.c
@@ -1,11 +1,11 @@
-#include "pins.h"
-#include "digio.h"
 #include <avr/io.h>
-
-// 2018-02-10: disabled pwm on pins 6,7,8. I need that timer to read the sonars
+#include "pins.h"
+//libreria pin digitali
+//Da rivedere mask
+//Completare alcuni associati a pin 0
 const Pin pins[] =
   {
-    //0
+//PinAVR 2 PinBoard 0
     {
       .in_register=&PINE,
       .out_register=&PORTE,
@@ -15,7 +15,7 @@ const Pin pins[] =
       .oc_register=0,
       .com_mask=0
     },
-    //1
+//PinAVR 3 PinBoard 1
     {
       .in_register=&PINE,
       .out_register=&PORTE,
@@ -25,7 +25,7 @@ const Pin pins[] =
       .oc_register=0,
       .com_mask=0
     },
-    //2
+//PinAVR 6 PinBoard 2
     {
       .in_register=&PINE,
       .out_register=&PORTE,
@@ -35,7 +35,7 @@ const Pin pins[] =
       .oc_register=&OCR3BL,
       .com_mask=(1<<COM3B0)|(1<<COM3B1)
     },
-    //3
+//PinAVR 7 PinBoard 3
     {
       .in_register=&PINE,
       .out_register=&PORTE,
@@ -45,7 +45,7 @@ const Pin pins[] =
       .oc_register=&OCR3CL,
       .com_mask=(1<<COM3C0)|(1<<COM3C1)
     },
-    //4
+//PinAVR 1 PinBoard 4
     {
       .in_register=&PING,
       .out_register=&PORTG,
@@ -55,7 +55,7 @@ const Pin pins[] =
       .oc_register=&OCR0B,
       .com_mask=(1<<COM0B0)|(1<<COM0B1)
     },
-    //5
+//PinAVR 5 PinBoard 5
     {
       .in_register=&PINE,
       .out_register=&PORTE,
@@ -65,46 +65,37 @@ const Pin pins[] =
       .oc_register=&OCR3AL,
       .com_mask=(1<<COM3A0)|(1<<COM3A1)
     },
-    //6
+//PinAVR 15 PinBoard 6
     {
       .in_register=&PINH,
       .out_register=&PORTH,
       .dir_register=&DDRH,
       .bit=3,
-      /* .tcc_register=&TCCR4A, */
-      /* .oc_register=&OCR4AL, */
-      /* .com_mask=(1<<COM4A0)|(1<<COM4A1) */
-      .tcc_register=0,
-      .oc_register=0,
-      .com_mask=0
+      .tcc_register=&TCCR4A, 
+      .oc_register=&OCR4AL,
+      .com_mask=(1<<COM4A0)|(1<<COM4A1)
     },
-    //7
+//PinAVR 16 PinBoard 7
     {
       .in_register=&PINH,
       .out_register=&PORTH,
       .dir_register=&DDRH,
       .bit=4,
-      /* .tcc_register=&TCCR4A, */
-      /* .oc_register=&OCR4BL, */
-      /* .com_mask=(1<<COM4B0)|(1<<COM4B1) */
-      .tcc_register=0,
-      .oc_register=0,
-      .com_mask=0
+      .tcc_register=&TCCR4A,
+      .oc_register=&OCR4BL, 
+      .com_mask=(1<<COM4B0)|(1<<COM4B1)
     },
-    //8
+//PinAVR 17 PinBoard 8
     {
       .in_register=&PINH,
       .out_register=&PORTH,
       .dir_register=&DDRH,
       .bit=5,
-      /* .tcc_register=&TCCR4A, */
-      /* .oc_register=&OCR4CL, */
-      /* .com_mask=(1<<COM4C0)|(1<<COM4C1) */
-      .tcc_register=0,
-      .oc_register=0,
-      .com_mask=-0
+      .tcc_register=&TCCR4A,
+      .oc_register=&OCR4CL, 
+      .com_mask=(1<<COM4C0)|(1<<COM4C1) 
     },
-    //9
+//PinAVR 18 PinBoard 9
     {
       .in_register=&PINH,
       .out_register=&PORTH,
@@ -114,7 +105,7 @@ const Pin pins[] =
       .oc_register=&OCR2B,
       .com_mask=(1<<COM2B0)|(1<<COM2B1)
     },
-    //10
+//PinAVR 23 PinBoard 10
     {
       .in_register=&PINB,
       .out_register=&PORTB,
@@ -124,7 +115,7 @@ const Pin pins[] =
       .oc_register=&OCR2A,
       .com_mask=(1<<COM2A0)|(1<<COM2A1)
     },
-    //11
+//PinAVR 24 PinBoard 11
     {
       .in_register=&PINB,
       .out_register=&PORTB,
@@ -134,7 +125,7 @@ const Pin pins[] =
       .oc_register=&OCR1AL,
       .com_mask=(1<<COM1A0)|(1<<COM1A1)
     },
-    //12
+//PinAVR 25 PinBoard 12
     {
       .in_register=&PINB,
       .out_register=&PORTB,
@@ -144,7 +135,7 @@ const Pin pins[] =
       .oc_register=&OCR1BL,
       .com_mask=(1<<COM1B0)|(1<<COM1B1)
     },
-    //13
+//PinAVR 26 PinBoard 13 OC0A/OC1C
     {
       .in_register=&PINB,
       .out_register=&PORTB,
@@ -154,4 +145,110 @@ const Pin pins[] =
       .oc_register=&OCR1CL,
       .com_mask=(1<<COM1C0)|(1<<COM1C1)
     }
-  };
+};
+
+
+//libreria pin digitali
+//Da rivedere mask
+//Completare alcuni associati a pin 0
+const Pin_analog pins_analog[] =
+  {
+//Pin analogici (ADC)
+//Ogni pin identifica un campo MUX di ADMUX MUXn + ->ADCSRB.MUX5
+	//PinAVR 97 PinBoard 0
+	{
+		//MUX5:0 = 000000
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 96 PinBoard 1
+	{
+		//MUX5:0=000001
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 95 PinBoard 2
+	{
+		//MUX5:0=000010
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 94 PinBoard 3
+	{
+		//MUX5:0=000011
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 93 PinBoard 4
+	{
+		//MUX5:0=000100
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 92 PinBoard 5
+	{
+		//MUX5:0=000101
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 91 PinBoard 6
+	{
+		//MUX5:0=000110
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 90 PinBoard 7
+	{
+		//MUX5:0=000111
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(0<<MUX5)
+	},
+	//PinAVR 89 PinBoard 8
+	{
+		//MUX5:0=100000
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 88 PinBoard 9
+	{
+		//MUX5:0=100001
+		.select_adc_mux=(0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 87 PinBoard 10
+	{
+		//MUX5:0=100010
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 86 PinBoard 11
+	{
+		//MUX5:0=100011
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 85 PinBoard 12
+	{
+		//MUX5:0=100100
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 84 PinBoard 13
+	{
+		//MUX5:0=100101
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 83 PinBoard 14
+	{
+		//MUX5:0=100110
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	},
+	//PinAVR 82 PinBoard 15
+	{
+		//MUX5:0=100111
+		.select_adc_mux= (0<<MUX4)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0),
+		.select_adc_adcsrb=(1<<MUX5)
+	}
+};
