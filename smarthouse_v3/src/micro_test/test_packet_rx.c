badfe5fca89d152040744a90d5a3df086b372582
Ubuntu 64-bit <diletta.lanini@gmail.com>
2019-11-13 03:48:33 -0800

    testSend

---
+++ smarthouse_v3/src/micro_test/test_packet_rx.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "packet_handler.h"
+#include "smarthouse_packets.h"
+#include "uart.h"
+#include "delay.h"
+#include <string.h>
+#include "smarthouse_globals.h"
+
+
+static struct UART* uart;
+static PacketHandler packet_handler;
+
+static PacketHeader* _initializeBuffer( PacketType type,
+                                        PacketSize size,
+                                        void* args )
+{
+	PacketHandler* handler=(PacketHandler*) args;
+	return (PacketHeader*) handler->rx_current_packet;
+}
+
+static PacketStatus _onReceive(PacketHeader *packet, void* arg)
+{
+	PacketHandler *handler=(PacketHandler*)arg;
+//arg deve essere un riferimento alla struc di globals
+//packet è il packetheader di quel pacchetto (risultato di intiaizeBuffer) che è stato ricevuto e va copiato in memoria
+	memcpy(handler, packet, packet->size);
+	return Success;
+}
+
+void flushBuffers(void) {
+  // we read all what we have to read
+  while (UART_rxBufferFull(uart))
+    PacketHandler_rxByte(&packet_handler, UART_getChar(uart));
+  // we write all what we have to write
+  while (packet_handler.tx_size)
+    UART_putChar(uart, PacketHandler_txByte(&packet_handler));
+}
+
+int main(int argc, char**argv)
+{
+	int seq = 0;
+	uart = UART_init(0, 115200);
+	PacketHandler_initialize(&packet_handler);
+	PacketOperations packet_ops = 
+	{
+		TEST_PACKET_ID,
+		sizeof(TestPacket),
+		_initializeBuffer,
+		(void*)&packet_handler,
+		_onReceive,
+		(void*)&packet_handler
+	};
+	PacketHandler_installPacket(&packet_handler, &packet_ops);
+  	while(1)
+	{
+		while (PacketHandler_sendPacket(&packet_handler, (PacketHeader*) & test) != Success)
+		{
+			flushBuffers();
+		}
+	test.header.seq = seq;
+	seq++;
+	}
+	delayMs(100);
+}
