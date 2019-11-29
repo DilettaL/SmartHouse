77a6e5d916c06252ed04b3a7aba0ad3e579b0331
Diletta Lanini <diletta.lanini@gmail.com>
2019-11-29 02:24:50 -0800

    test_packet

--- smarthouse_v4/src/smarthouse_host/smarthouse_host.c
+++ smarthouse_v4/src/smarthouse_host/smarthouse_host.c
@@ -6,11 +6,6 @@
 #include "packet_handler.h"
 #include "smarthouse_packets.h"
 
-///***
-#include <pthread.h>
-#include <stdlib.h>
-///***
-
 struct UART* uart;
 PacketHandler packet_handler;
 
@@ -45,37 +40,11 @@ PacketOperations test_ops = {
 	0
 };
 
-///****
-int fd;
-
-void *listen_serial()
-{
-volatile int packet_complete =0;
-		while ( !packet_complete ) 
-		{
-			uint8_t c;
-			int n=read (fd, &c, 1);
-			if (n) 
-			{
-				PacketStatus status = PacketHandler_rxByte(&packet_handler, c);
-				if (status<0)
-					printf("%d",status);
-				fflush(stdout);
-				packet_complete = (status==SyncChecksum);
-			}
-		}
-	return NULL;
-}
-
 ///*****
 int main (int argc, char **argv)
 {
 	assert(argc>1);
-//****
-pthread_t serial;
-pthread_attr_t attr_serial;
-pthread_attr_init(&attr_serial);
-fd=serial_open(argv[1]);
+	int fd=serial_open(argv[1]);
 	if(fd<0)
 		return 0;
 	if (serial_set_interface_attribs(fd, 115200, 0) <0)
@@ -83,22 +52,13 @@ fd=serial_open(argv[1]);
 	serial_set_blocking(fd, 1); 
 	if  (! fd)
 		return 0;
-	
-
-int control=pthread_create(&serial, &attr_serial, &listen_serial, NULL);
-if (control)
-{
-    	printf("ERROR; return code from pthread_create()\n");
-        exit(-1);
-}
-//****
 	PacketHandler_initialize(&packet_handler);
 	PacketHandler_installPacket(&packet_handler, &test_ops);
 
 	test.prova=8; 
 	for (int i=0; i<1000; ++i)
 	{
-/*		volatile int packet_complete =0;
+		volatile int packet_complete =0;
 		while ( !packet_complete ) 
 		{
 			uint8_t c;
@@ -112,37 +72,16 @@ if (control)
 				packet_complete = (status==SyncChecksum);
 			}
 		}
-*/
-
-test.prova=8;
-PacketHandler_sendPacket(&packet_handler, (PacketHeader*)&test);
-printf("%d]\tHost Transmission (mi aspetto 8): test-> %d\n", i, test.prova);
-
-while(packet_handler.tx_size)
-		{
-			uint8_t c=PacketHandler_txByte(&packet_handler);
-			ssize_t res = write(fd,&c,1);
-		usleep(10);
-		}
- 
-	//****
-int count;
-while( (count=pthread_join(serial, NULL)) != 0 )
-{
-	printf ("count1: %d\n", count);
-	PacketHandler_sendPacket(&packet_handler, (PacketHeader*)&test);
-	printf ("count2: %d\n", count);	
+		test.prova=8;
+		PacketHandler_sendPacket(&packet_handler, (PacketHeader*)&test);
+		printf("%d]\tHost Transmission (mi aspetto 8): test-> %d\n", i, test.prova);
 		while(packet_handler.tx_size)
 		{
 			uint8_t c=PacketHandler_txByte(&packet_handler);
 			ssize_t res = write(fd,&c,1);
-		usleep(10);
+			usleep(10);
 		}
-}
-
-//****
-	}
-pthread_attr_destroy(&attr_serial);	
+	} 
 	return 0;
 }
 
