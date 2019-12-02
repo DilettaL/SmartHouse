0ef5c1bdbba1f40e459ea8939912f86e5043217f
Luca Primomo <primomo.luca@gmail.com>
2019-12-02 17:29:49 +0100

    digitali ok

--- smarthouse_v4/src/smarthouse_firmware/smarthouse_functions.c
+++ smarthouse_v4/src/smarthouse_firmware/smarthouse_functions.c
@@ -24,7 +24,7 @@ void Dimmer(uint8_t pin, uint8_t intensity)
 void InputDigital(uint8_t pin)
 {
 	DigIO_setDirection(pin, Input);
-	DigIO_setValue(pin, 1);
+	//DigIO_setValue(pin, 1);
 	digital_status.inputs=DigIO_getValue(pin);
 }
 
