e590b7e95db91b76ccb9ab4eac99675ef0423e77
Diletta Lanini <diletta.lanini@gmail.com>
2019-12-02 06:47:36 -0800

    modify gitignore

---
+++ smarthouse_v4/src/smarthouse_host/smarthouse_host_globals.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "smarthouse_packets.h"
+
+extern TestConfigPacket test_config;
+extern TestStatusPacket test_status;
+
+extern DigitalConfigPacket digital_config;
+extern DigitalStatusPacket digital_status;
